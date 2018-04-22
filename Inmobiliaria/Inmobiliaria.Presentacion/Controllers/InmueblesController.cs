9fa81ecd9212db326c093ca7007869fd6ed99430
Jhon <jhoncastrillon9@gmail.com>
2018-04-22 00:32:36 -0500

    Stars Appis

--- Inmobiliaria/Inmobiliaria.Presentacion/Controllers/InmueblesController.cs
+++ Inmobiliaria/Inmobiliaria.Presentacion/Controllers/InmueblesController.cs
@@ -1,23 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Web;
 using System.Web.Mvc;
+using Inmobiliaria.Dominio;
+using Newtonsoft.Json;
+
 
 namespace Inmobiliaria.Presentacion.Controllers
 {
     public class InmueblesController : Controller
     {
-        // GET: Inmuebles
+        //Instanciamos Para consumir Servicio
+        HttpClient clienteHttp = new HttpClient();
+
         // GET: Inmuebles
         public ActionResult Index()
         {
+            //Indicamos donde tenemos las APi la Direccion
+            clienteHttp.BaseAddress = new Uri("http://localhost:53650/api/Inmuebles/");
+            //Consumimos apis y guardamos resultados
+            var request = clienteHttp.GetAsync("api/Inmuebles").Result;
 
+            //variables para el view
             ViewBag.ubicacion = "Inicio / Inmuebles / Listado";
             ViewBag.titulo = "Lista de Inmuebles!";
             ViewBag.icon = "home";
 
+            //Si la respuesta es afirmativa (Devolvio algo)
+            if (request.IsSuccessStatusCode)
+            {
+                //Leemos el resultado
+                var resulstring = request.Content.ReadAsStringAsync().Result;
+
+                var ListadoInmuebles = JsonConvert.DeserializeObject<List<InmueblesDTO>>(resulstring);
+
+
+
+
+                return View(ListadoInmuebles);
+            }
+
             return View();
+
         }
     }
 }
---
+++ Inmobiliaria/Inmobiliaria.Servicios/Controllers/InmueblesController.cs
@@ -0,0 +1,31 @@
+﻿using Inmobiliaria.Datos.Modelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Inmobiliaria.Servicios.Controllers
+{
+    public class InmueblesController : ApiController
+    {
+        Inmobiliaria_DesarrolloEntities BD = new Inmobiliaria_DesarrolloEntities();
+
+        [HttpGet]
+        public IEnumerable<Inmuebles> Get()
+        {    
+            var LInmuebles = BD.Inmuebles.ToList();
+
+            return LInmuebles;
+        }
+
+        [HttpGet]
+        public Inmuebles Get(int id)
+        {
+            var Inmuebles = BD.Inmuebles.FirstOrDefault(x=> x.Id == id);
+
+            return Inmuebles;
+        }
+    }
+}
